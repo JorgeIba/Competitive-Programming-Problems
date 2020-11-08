a290396b57f82f06c66b28dbdda1ea3824bbe08c
Jorge Luis Ibáñez <39895741+JorgeIba@users.noreply.github.com>
2020-11-08 01:54:25 -0600

    Delete Fygon.py

--- Fygon.py
+++
@@ -1,8 +0,0 @@
-import os
-from fractions import Fraction as Frac
-
-code = ""
-with open(os.path.join(sys.path[0], "my_file.txt"), "r") as f:
-    code = f.read().replace("lag", "ans+=1")
-
-print(code)
